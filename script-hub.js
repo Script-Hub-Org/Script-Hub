cee7950cc3aa01f019e74d0a93d1789048ecf348
xream <xream@icloud.com>
2023-07-10 20:55:12 +0800

    [!] frontend

--- script-hub.js
+++ script-hub.js
@@ -9,7 +9,7 @@ const html = `
   <head>
     <meta charset="UTF-8" />
     <link rel="icon" type="image/png" href="https://raw.githubusercontent.com/Script-Hub-Org/Script-Hub/main/assets/icon.png" />
-    <link rel="apple-touch-icon"href="https://raw.githubusercontent.com/Script-Hub-Org/Script-Hub/main/assets/icon-dark.png">
+    <link rel="apple-touch-icon" href="https://raw.githubusercontent.com/Script-Hub-Org/Script-Hub/main/assets/icon-dark.png">
     <link rel="stylesheet" href="https://unpkg.com/simpledotcss/simple.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Script Hub</title>
