fa477fe4c740c7eb54b0f6b618bdedd607a3a11a
xream <xream@icloud.com>
2023-07-16 22:59:24 +0800

    [!] preview

--- preview.js
+++ preview.js
@@ -1,5 +1,6 @@
-import { readFile } from 'node:fs/promises'
-import Koa from 'koa'
+const fs = require('fs')
+const path = require('path')
+const Koa = require('Koa')
 
 const PORT = process.env.PORT || 9000
 const BETA_PORT = process.env.BETA_PORT || 9001
@@ -8,11 +9,14 @@ const HOST = process.env.HOST || '0.0.0.0'
 const app = new Koa()
 
 app.use(async ctx => {
-  const filePath = new URL('./script-hub.js', import.meta.url)
-  const content = await readFile(filePath, { encoding: 'utf8' })
+  const filePath = path.join(__dirname, './script-hub.js')
+  const content = fs.readFileSync(filePath, { encoding: 'utf8' })
   ctx.type = 'html'
   // ctx.body = content.match(/<!DOCTYPE html>([\s\S]*?)<\/html>/i)[1]
-  ctx.body = content.match(/<!DOCTYPE html>([\s\S]*?<body style="margin-bottom: 80px;"><script>)/i)[1] + '</script><script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>'  + content.match(/(<div id="app">[\s\S]*?)<\/html>/i)[1]
+  ctx.body =
+    content.match(/<!DOCTYPE html>([\s\S]*?<body style="margin-bottom: 80px;"><script>)/i)[1] +
+    '</script><script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>' +
+    content.match(/(<div id="app">[\s\S]*?)<\/html>/i)[1]
 })
 
 app.listen(PORT, HOST, async ctx => {
@@ -22,11 +26,14 @@ app.listen(PORT, HOST, async ctx => {
 const appBeta = new Koa()
 
 appBeta.use(async ctx => {
-  const filePath = new URL('./script-hub.beta.js', import.meta.url)
-  const content = await readFile(filePath, { encoding: 'utf8' })
+  const filePath = path.join(__dirname, './script-hub.beta.js')
+  const content = fs.readFile(filePath, { encoding: 'utf8' })
   ctx.type = 'html'
   // ctx.body = content.match(/<!DOCTYPE html>([\s\S]*?)<\/html>/i)[1]
-  ctx.body = content.match(/<!DOCTYPE html>([\s\S]*?<body style="margin-bottom: 80px;"><script>)/i)[1] + '</script><script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>'  + content.match(/(<div id="app">[\s\S]*?)<\/html>/i)[1]
+  ctx.body =
+    content.match(/<!DOCTYPE html>([\s\S]*?<body style="margin-bottom: 80px;"><script>)/i)[1] +
+    '</script><script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>' +
+    content.match(/(<div id="app">[\s\S]*?)<\/html>/i)[1]
 })
 
 appBeta.listen(BETA_PORT, HOST, async ctx => {
