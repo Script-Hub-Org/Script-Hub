584daf03b7069120f4c0ee9512c8e54bb06ae708
xream <xream@icloud.com>
2023-07-25 14:39:03 +0800

    [!] SurgeModuleTool

--- SurgeModuleTool.js
+++ SurgeModuleTool.js
@@ -1,7 +1,10 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: blue; icon-glyph: cloud-download-alt;
-let ToolVersion = '1.98'
+
+// prettier-ignore
+let ToolVersion = "1.98";
+
 async function delay(milliseconds) {
   var before = Date.now()
   while (Date.now() < before + milliseconds) {}
