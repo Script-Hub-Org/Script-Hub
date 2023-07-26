c9ccba8b005d13eb4e9f19735b86f4572527e1f4
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-07-27 01:21:24 +0800

    更新 rule-parser.beta.js

--- rule-parser.beta.js
+++ rule-parser.beta.js
@@ -2,6 +2,9 @@
    支持QX & Surge & Clash 规则集解析
    适用app: Surge Shadowrocket Stash Loon
 ***************************/
+
+const $ = new Env("rule-parser");
+
 //目标app
 const isEgern = 'object' == typeof egern;
 const isLanceX = 'undefined' != typeof $native;
@@ -14,17 +17,6 @@ const isSurgeiOS = 'undefined' !== typeof $environment && $environment['surge-ve
 const isShadowrocket = 'undefined' !== typeof $rocket;
 const isLooniOS = 'undefined' != typeof $loon;
 
-//本机app
-const isEgernL = 'object' == typeof egern;
-const isLanceXL = 'undefined' != typeof $native;
-if (isEgernL || isLanceXL){
-	$environment = {"language":"zh-Hans","system":"iOS","surge-build":"2806","surge-version":"5.20.0"}
-};
-const isStashiOSL = 'undefined' !== typeof $environment && $environment['stash-version'];
-const isSurgeiOSL = 'undefined' !== typeof $environment && $environment['surge-version'];
-const isShadowrocketL = 'undefined' !== typeof $rocket;
-const isLooniOSL = 'undefined' != typeof $loon;
-
 const url = $request.url;
 var req = url.split(/file\/_start_\//)[1].split(/\/_end_\//)[0];
 	//console.log("原始链接：" + req);
@@ -36,7 +28,7 @@ resFile.substring(0,resFile.lastIndexOf('.'));
 
 //获取参数
 const queryObject = parseQueryString(urlArg);
-//console.log("参数:" + toStr(queryObject));
+//console.log("参数:" + $.toStr(queryObject));
 var Rin0 = queryObject.y != undefined ? queryObject.y.split("+") : null;
 var Rout0 = queryObject.x != undefined ? queryObject.x.split("+") : null;
 var ipNoResolve = istrue(queryObject.nore);
@@ -51,7 +43,7 @@ var evUrlmodi = queryObject.evalUrlmodi;
 //缓存有效期相关
 var currentTime = new Date();
 var seconds = Math.floor(currentTime.getTime() / 1000); // 将毫秒转换为秒
-var boxjsSetExp = getval("Parser_cache_exp") ?? "1";
+var boxjsSetExp = $.getval("Parser_cache_exp") ?? "1";
 //设置有效期时间
 var expirationTime
 if (cachExp != null){
@@ -61,62 +53,62 @@ if (cachExp != null){
 };
 //console.log(expirationTime);
 var nCache = [{"url":"","body":"","time":""}];
-var oCache = getval("parser_cache");
+var oCache = $.getval("parser_cache");
 //检查是否有缓存
 if (oCache != "" && oCache != null){
-  oCache = toObj(oCache);
+  oCache = $.toObj(oCache);
 }else{oCache = null;};
 
 !(async () => {
 	
 if (evUrlori){
-evUrlori = await http(evUrlori);
+evUrlori = (await $.http.get(evUrlori)).body;
 };
 if (evUrlmodi){
-evUrlmodi = await http(evUrlmodi);
+evUrlmodi = (await $.http.get(evUrlmodi)).body;
 };
 
 let body
 
   if (noCache == true){
-	body = await http(req);
+	body = (await $.http.get(req)).body;
 }else if (oCache == null){
     //console.log("一个缓存也没有")
-  body = await http(req);
+  body = (await $.http.get(req)).body;
   nCache[0].url = req;
   nCache[0].body = body;
   nCache[0].time = seconds;
-  setval(toStr(nCache), 'parser_cache');
+  $.setval($.toStr(nCache), 'parser_cache');
   }else{
     //删除大于一天的缓存防止缓存越来越大
     oCache = oCache.filter(obj => {
   return seconds - obj.time < 86400 ;
 });
-setval(toStr(oCache), 'parser_cache');
+$.setval($.toStr(oCache), 'parser_cache');
 
  if (!oCache.some(obj => obj.url === req)){
      //console.log("有缓存但是没有这个URL的")
-  body = await http(req);
+  body = (await $.http.get(req)).body;
   nCache[0].url = req;
   nCache[0].body = body;
   nCache[0].time = seconds;
   var mergedCache = oCache.concat(nCache);
-setval(toStr(mergedCache), 'parser_cache');
+$.setval($.toStr(mergedCache), 'parser_cache');
   }else if (oCache.some(obj => obj.url === req)){
     const objIndex = oCache.findIndex(obj => obj.url === req);
     if (seconds - oCache[objIndex].time > expirationTime){
       //console.log("有缓存且有url,但是过期了")
-  body = await http(req);
+  body = (await $.http.get(req)).body;
   oCache[objIndex].body = body;
   oCache[objIndex].time = seconds;
-setval(toStr(oCache), 'parser_cache');
+$.setval($.toStr(oCache), 'parser_cache');
     }else{
       //console.log("有缓存且有url且没过期")
     if (oCache[objIndex].body == null || oCache[objIndex].body == ""){
         //console.log("但是body为null")
-        body = await http(req);
+        body = (await $.http.get(req)).body;
         oCache[objIndex].body = body;
-        oCache[objIndex].time = seconds;        setval(toStr(oCache), "parser_cache");
+        oCache[objIndex].time = seconds;        $.setval($.toStr(oCache), "parser_cache");
     }else{
         //console.log("获取到缓存body")
         body = oCache[objIndex].body;
@@ -127,14 +119,6 @@ setval(toStr(oCache), 'parser_cache');
   
 eval(evJsori);
 eval(evUrlori);
-
-//判断是否断网
-if(body == null || body == ""){
-	
-	 notify(`规则集转换："${resFileName}"未获取到body`,"请检查网络及节点是否畅通\n" + "源链接为" + $request.url,"认为是bug?点击通知反馈","https://t.me/zhangpeifu")
- $done({ response: { status: 404 ,body:{} } });
-
-}else{//以下开始规则集解析
 
 	body = body.match(/[^\r\n]+/g);
 	
@@ -265,12 +249,11 @@ body = `${ruleSet}`.replace(/t&zd;/g,',').replace(/ ;#/g," ");
 eval(evJsmodi);
 eval(evUrlmodi);
 
- $done({ response: { status: 200 ,body:body ,headers: {'Content-Type': 'text/plain; charset=utf-8'} } });
-}//判断是否断网的反括号
+ $.done({ response: { status: 200 ,body:body ,headers: {'Content-Type': 'text/plain; charset=utf-8'} } });
 
 })()
 .catch((e) => {
-		notify(`Script Hub: 规则集转换`,`${e}`,'','https://t.me/zhetengsha_group');
+		$.msg(`Script Hub: 规则集转换`,`${e}`,'','https://t.me/zhetengsha_group');
 		result = {
       response: {
         status: 500,
@@ -284,46 +267,9 @@ eval(evUrlmodi);
         },
       },
     }
+	$.done(result);
 	})
-
-function notify ( title , subt , desc , opts ){
-	if (isShadowrocketL || isLooniOSL){		$notification.post(title,subt,desc,opts);
-	}else{
-		$notification.post(title,subt,desc,{url:opts});};
-};
-
-function getval(key) {
-          return $persistentStore.read(key)
-    };
-
-function setval(val, key) {
-          return $persistentStore.write(val, key)
-    };
-		
-function toObj(str, defaultValue = null) {
-      try {
-        return JSON.parse(str)
-      } catch {
-        return defaultValue
-      }
-    };
-
-function toStr(obj, defaultValue = null) {
-      try {
-        return JSON.stringify(obj)
-      } catch {
-        return defaultValue
-      }
-    };
 	
-function http(req) {
-  return new Promise((resolve, reject) =>
-    $httpClient.get(req, (err, resp,data) => {
-  resolve(data)
-  })
-)
-};
-
 function istrue(str) {
 	if (str == true || str == 1 || str == "true"|| str == "1"){
 		return true;
@@ -344,3 +290,7 @@ function parseQueryString(url) {
 
   return params;
 };
+
+
+
+function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,a)=>{s.call(this,t,(t,s,r)=>{t?a(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.encoding="utf-8",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`🔔${this.name}, 开始!`)}getEnv(){return"undefined"!=typeof $environment&&$environment["surge-version"]?"Surge":"undefined"!=typeof $environment&&$environment["stash-version"]?"Stash":"undefined"!=typeof module&&module.exports?"Node.js":"undefined"!=typeof $task?"Quantumult X":"undefined"!=typeof $loon?"Loon":"undefined"!=typeof $rocket?"Shadowrocket":void 0}isNode(){return"Node.js"===this.getEnv()}isQuanX(){return"Quantumult X"===this.getEnv()}isSurge(){return"Surge"===this.getEnv()}isLoon(){return"Loon"===this.getEnv()}isShadowrocket(){return"Shadowrocket"===this.getEnv()}isStash(){return"Stash"===this.getEnv()}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const a=this.getdata(t);if(a)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,a)=>e(a))})}runScript(t,e){return new Promise(s=>{let a=this.getdata("@chavy_boxjs_userCfgs.httpapi");a=a?a.replace(/\n/g,"").trim():a;let r=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");r=r?1*r:20,r=e&&e.timeout?e.timeout:r;const[i,o]=a.split("@"),n={url:`http://${o}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:r},headers:{"X-Key":i,Accept:"*/*"},timeout:r};this.post(n,(t,e,a)=>s(a))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),a=!s&&this.fs.existsSync(e);if(!s&&!a)return{};{const a=s?t:e;try{return JSON.parse(this.fs.readFileSync(a))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),a=!s&&this.fs.existsSync(e),r=JSON.stringify(this.data);s?this.fs.writeFileSync(t,r):a?this.fs.writeFileSync(e,r):this.fs.writeFileSync(t,r)}}lodash_get(t,e,s){const a=e.replace(/\[(\d+)\]/g,".$1").split(".");let r=t;for(const t of a)if(r=Object(r)[t],void 0===r)return s;return r}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,a)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[a+1])>>0==+e[a+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,a]=/^@(.*?)\.(.*?)$/.exec(t),r=s?this.getval(s):"";if(r)try{const t=JSON.parse(r);e=t?this.lodash_get(t,a,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,a,r]=/^@(.*?)\.(.*?)$/.exec(e),i=this.getval(a),o=a?"null"===i?null:i||"{}":"{}";try{const e=JSON.parse(o);this.lodash_set(e,r,t),s=this.setval(JSON.stringify(e),a)}catch(e){const i={};this.lodash_set(i,r,t),s=this.setval(JSON.stringify(i),a)}}else s=this.setval(t,e);return s}getval(t){switch(this.getEnv()){case"Surge":case"Loon":case"Stash":case"Shadowrocket":return $persistentStore.read(t);case"Quantumult X":return $prefs.valueForKey(t);case"Node.js":return this.data=this.loaddata(),this.data[t];default:return this.data&&this.data[t]||null}}setval(t,e){switch(this.getEnv()){case"Surge":case"Loon":case"Stash":case"Shadowrocket":return $persistentStore.write(t,e);case"Quantumult X":return $prefs.setValueForKey(t,e);case"Node.js":return this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0;default:return this.data&&this.data[e]||null}}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){switch(t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"],delete t.headers["content-type"],delete t.headers["content-length"]),t.params&&(t.url+="?"+this.queryStr(t.params)),this.getEnv()){case"Surge":case"Loon":case"Stash":case"Shadowrocket":default:this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.get(t,(t,s,a)=>{!t&&s&&(s.body=a,s.statusCode=s.status?s.status:s.statusCode,s.status=s.statusCode),e(t,s,a)});break;case"Quantumult X":this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:a,headers:r,body:i,bodyBytes:o}=t;e(null,{status:s,statusCode:a,headers:r,body:i,bodyBytes:o},i,o)},t=>e(t&&t.error||"UndefinedError"));break;case"Node.js":let s=require("iconv-lite");this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{if(t.headers["set-cookie"]){const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();s&&this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:a,statusCode:r,headers:i,rawBody:o}=t,n=s.decode(o,this.encoding);e(null,{status:a,statusCode:r,headers:i,rawBody:o,body:n},n)},t=>{const{message:a,response:r}=t;e(a,r,r&&s.decode(r.rawBody,this.encoding))})}}post(t,e=(()=>{})){const s=t.method?t.method.toLocaleLowerCase():"post";switch(t.body&&t.headers&&!t.headers["Content-Type"]&&!t.headers["content-type"]&&(t.headers["content-type"]="application/x-www-form-urlencoded"),t.headers&&(delete t.headers["Content-Length"],delete t.headers["content-length"]),this.getEnv()){case"Surge":case"Loon":case"Stash":case"Shadowrocket":default:this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient[s](t,(t,s,a)=>{!t&&s&&(s.body=a,s.statusCode=s.status?s.status:s.statusCode,s.status=s.statusCode),e(t,s,a)});break;case"Quantumult X":t.method=s,this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:a,headers:r,body:i,bodyBytes:o}=t;e(null,{status:s,statusCode:a,headers:r,body:i,bodyBytes:o},i,o)},t=>e(t&&t.error||"UndefinedError"));break;case"Node.js":let a=require("iconv-lite");this.initGotEnv(t);const{url:r,...i}=t;this.got[s](r,i).then(t=>{const{statusCode:s,statusCode:r,headers:i,rawBody:o}=t,n=a.decode(o,this.encoding);e(null,{status:s,statusCode:r,headers:i,rawBody:o,body:n},n)},t=>{const{message:s,response:r}=t;e(s,r,r&&a.decode(r.rawBody,this.encoding))})}}time(t,e=null){const s=e?new Date(e):new Date;let a={"M+":s.getMonth()+1,"d+":s.getDate(),"H+":s.getHours(),"m+":s.getMinutes(),"s+":s.getSeconds(),"q+":Math.floor((s.getMonth()+3)/3),S:s.getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,(s.getFullYear()+"").substr(4-RegExp.$1.length)));for(let e in a)new RegExp("("+e+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?a[e]:("00"+a[e]).substr((""+a[e]).length)));return t}queryStr(t){let e="";for(const s in t){let a=t[s];null!=a&&""!==a&&("object"==typeof a&&(a=JSON.stringify(a)),e+=`${s}=${a}&`)}return e=e.substring(0,e.length-1),e}msg(e=t,s="",a="",r){const i=t=>{switch(typeof t){case void 0:return t;case"string":switch(this.getEnv()){case"Surge":case"Stash":default:return{url:t};case"Loon":case"Shadowrocket":return t;case"Quantumult X":return{"open-url":t};case"Node.js":return}case"object":switch(this.getEnv()){case"Surge":case"Stash":case"Shadowrocket":default:{let e=t.url||t.openUrl||t["open-url"];return{url:e}}case"Loon":{let e=t.openUrl||t.url||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}case"Quantumult X":{let e=t["open-url"]||t.url||t.openUrl,s=t["media-url"]||t.mediaUrl,a=t["update-pasteboard"]||t.updatePasteboard;return{"open-url":e,"media-url":s,"update-pasteboard":a}}case"Node.js":return}default:return}};if(!this.isMute)switch(this.getEnv()){case"Surge":case"Loon":case"Stash":case"Shadowrocket":default:$notification.post(e,s,a,i(r));break;case"Quantumult X":$notify(e,s,a,i(r));break;case"Node.js":}if(!this.isMuteLog){let t=["","==============📣系统通知📣=============="];t.push(e),s&&t.push(s),a&&t.push(a),console.log(t.join("\n")),this.logs=this.logs.concat(t)}}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){switch(this.getEnv()){case"Surge":case"Loon":case"Stash":case"Shadowrocket":case"Quantumult X":default:this.log("",`❗️${this.name}, 错误!`,t);break;case"Node.js":this.log("",`❗️${this.name}, 错误!`,t.stack)}}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;switch(this.log("",`🔔${this.name}, 结束! 🕛 ${s} 秒`),this.log(),this.getEnv()){case"Surge":case"Loon":case"Stash":case"Shadowrocket":case"Quantumult X":default:$done(t);break;case"Node.js":process.exit(1)}}}(t,e)}
