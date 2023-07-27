6ec0ef792c46e828ccf39f023518dc4dadefe680
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-07-27 21:12:09 +0800

    更新 Loon-Rewrite-Parser.js

--- Loon-Rewrite-Parser.js
+++ Loon-Rewrite-Parser.js
@@ -422,7 +422,7 @@ let rewType = "";      //302/307/header重写类型
 let scriptBox = [];    //存放脚本信息待下一步处理
 
 for await (var [y, x] of body.entries()) {
-	x = x.replace(/^ *(#|;|\/\/)/,'#').replace(/, *REJECT/i,',REJECT').replace(/ reject/i,' reject').replace(/(^[^#].+)\x20+\/\/.+/,"$1").replace(/(hostname|force-http-engine-hosts|skip-proxy|always-real-ip)\x20*=/,'$1=').replace(/ *, *enabled *= *false/,"");
+	x = x.replace(/^ *(#|;|\/\/)/,'#').replace(/, *REJECT/i,',REJECT').replace(/ reject/i,' reject').replace(/(^[^#].+)\x20+\/\/.+/,"$1").replace(/^ *(hostname|force-http-engine-hosts|skip-proxy|always-real-ip)\x20*=/,'$1=').replace(/ *, *enabled *= *false/,"");
 //去掉注释
 if(Pin0 != null)	{
 	for (let i=0; i < Pin0.length; i++) {
