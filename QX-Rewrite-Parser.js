f1bea6f6db0519456da56c58035d59773e3a2ad9
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-07-31 00:04:17 +0800

    更新 QX-Rewrite-Parser.js

--- QX-Rewrite-Parser.js
+++ QX-Rewrite-Parser.js
@@ -47,8 +47,9 @@ var compatibilityOnly = istrue(queryObject.compatibilityOnly);
 const iconStatus = $.getval("启用插件随机图标") ?? "启用";
 const iconReplace = $.getval("替换原始插件图标");
 const iconLibrary1 = $.getval("插件随机图标合集") ?? "Doraemon(100P)";
-const iconLibrary2 = iconLibrary1.split("(")[0];
+var iconLibrary2 = iconLibrary1.split("(")[0];
 const iconFormat = iconLibrary2.search(/gif/i) == -1 ? ".png" : ".gif";
+if (iconStatus == "禁用" && iconReplace == "禁用"){iconLibrary2 = "Doraemon"};
 
 var pluginPokemonIcon
 var pluginPokemonAuthor
@@ -349,6 +350,7 @@ const pluginIcon = icon;
 }else if (oCache == null){
     //$.log("一个缓存也没有")
   body = (await $.http.get(req)).body;
+$.log('body字节数:' + body.length + '字');
   nCache[0].url = req;
   nCache[0].body = body;
   nCache[0].time = seconds;
@@ -363,6 +365,7 @@ $.setjson(oCache, 'parser_cache');
  if (!oCache.some(obj => obj.url === req)){
      //$.log("有缓存但是没有这个URL的")
   body = (await $.http.get(req)).body;
+$.log('body字节数:' + body.length + '字');
   nCache[0].url = req;
   nCache[0].body = body;
   nCache[0].time = seconds;
@@ -373,6 +376,7 @@ $.setjson(mergedCache, 'parser_cache');
     if (seconds - oCache[objIndex].time > expirationTime){
       //$.log("有缓存且有url,但是过期了")
   body = (await $.http.get(req)).body;
+$.log('body字节数:' + body.length + '字');
   oCache[objIndex].body = body;
   oCache[objIndex].time = seconds;
 $.setjson(oCache, 'parser_cache');
@@ -381,6 +385,7 @@ $.setjson(oCache, 'parser_cache');
     if (oCache[objIndex].body == null || oCache[objIndex].body == ""){
         //$.log("但是body为null")
         body = (await $.http.get(req)).body;
+$.log('body字节数:' + body.length + '字');
         oCache[objIndex].body = body;
         oCache[objIndex].time = seconds;        $.setjson(oCache, "parser_cache");
     }else{
@@ -561,8 +566,6 @@ if (isLooniOS || isSurgeiOS || isShadowrocket){
             if (iconReplace == "启用"){
                 x = x.replace(/^#!icon *=.*/,pluginIcon);
             };
-			x = x.replace(/^(#!author *=).*/i,pluginPokemonAuthor)
-			x = x.replace(/^(#!homepage *=).*/i,pluginPokemonHomepage)
             pluginDesc.push(x);
 				
 			}else if (isLooniOS || isSurgeiOS || isShadowrocket){
@@ -845,20 +848,15 @@ if (isLooniOS){
     
     if (pluginDesc !="" && pluginDesc.search(/#! *name *=/) != -1){
         //没有图标的插入图标
-        if (pluginDesc.search(/#! *icon *= *.+/) == -1){
+        if (pluginDesc.search(/#! *icon *= *.*/) == -1){
         pluginDesc = pluginDesc + "\n" + pluginIcon;
             
-        }else{pluginDesc = pluginDesc;};
+        };
 		
-        //Pokemon没有作者的插入作者
-        if (iconLibrary2 == "Pokemon" && pluginDesc.search(/#! *author *= *.+/i) == -1){
-        pluginDesc = pluginDesc + "\n" + pluginPokemonAuthor;
-        }else{pluginDesc = pluginDesc;};
-		
-        //Pokemon没有homepage的插入homepage
-        if (iconLibrary2 == "Pokemon" && pluginDesc.search(/#! *homepage *= *.+/i) == -1){
-        pluginDesc = pluginDesc + "\n" + pluginPokemonHomepage;
-        }else{pluginDesc = pluginDesc;};
+        //Pokemon修改author和homepage为图鉴
+        if (iconLibrary2 == "Pokemon" && pluginDesc.search(/^#! *icon *= *.+Pokemon-[0-9]+\.png$/mi) != -1){
+        pluginDesc = pluginDesc.replace(/^#!(?:author|homepage) *= *.*/gmi,'') + "\n" + pluginPokemonAuthor + "\n" + pluginPokemonHomepage;
+        };
 		
     }else{
         if (iconLibrary2 == "Pokemon"){
@@ -867,9 +865,6 @@ if (isLooniOS){
                     pluginDesc = npluginDesc + "\n" + pluginIcon;
         };
     };
-    
-    if (iconReplace == "启用" && pluginDesc.search(/#!icon=/) == -1 ){
-        pluginDesc = pluginDesc + "\n" + pluginIcon};
     
 	script = (script[0] || '') && `[Script]\n\n${script.join("\n\n")}`;
 	
