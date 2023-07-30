b92e2e53a854fb3f15cce1e377a2328bd00202ac
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-07-31 00:04:02 +0800

    更新 QX-Rewrite-Parser.beta.js

--- QX-Rewrite-Parser.beta.js
+++ QX-Rewrite-Parser.beta.js
@@ -47,8 +47,9 @@ var compatibilityOnly = istrue(queryObject.compatibilityOnly);
 const iconStatus = $.getval("启用插件随机图标") ?? "启用";
 const iconReplace = $.getval("替换原始插件图标");
 const iconLibrary1 = $.getval("插件随机图标合集") ?? "Doraemon(100P)";
-const iconLibrary2 = iconLibrary1.split("(")[0];
+var iconLibrary2 = iconLibrary1.split("(")[0];
 const iconFormat = iconLibrary2.search(/gif/i) == -1 ? ".png" : ".gif";
+if (iconStatus == "禁用" && iconReplace == "禁用"){iconLibrary2 = "Doraemon"};
 
 var pluginPokemonIcon
 var pluginPokemonAuthor
@@ -565,8 +566,6 @@ if (isLooniOS || isSurgeiOS || isShadowrocket){
             if (iconReplace == "启用"){
                 x = x.replace(/^#!icon *=.*/,pluginIcon);
             };
-			x = x.replace(/^(#!author *=).*/i,pluginPokemonAuthor)
-			x = x.replace(/^(#!homepage *=).*/i,pluginPokemonHomepage)
             pluginDesc.push(x);
 				
 			}else if (isLooniOS || isSurgeiOS || isShadowrocket){
@@ -849,20 +848,15 @@ if (isLooniOS){
     
     if (pluginDesc !="" && pluginDesc.search(/#! *name *=/) != -1){
         //没有图标的插入图标
-        if (pluginDesc.search(/#! *icon *= *.+/) == -1){
+        if (pluginDesc.search(/#! *icon *= *.*/) == -1){
         pluginDesc = pluginDesc + "\n" + pluginIcon;
             
-        }else{pluginDesc = pluginDesc;};
+        };
 		
-        //Pokemon没有作者的插入作者
-        if (iconLibrary2 == "Pokemon" && pluginDesc.search(/#! *author *= *.+/i) == -1){
-        pluginDesc = pluginDesc + "\n" + pluginPokemonAuthor;
-        }else{pluginDesc = pluginDesc;};
-		
-        //Pokemon没有homepage的插入homepage
-        if (iconLibrary2 == "Pokemon" && pluginDesc.search(/#! *homepage *= *.+/i) == -1){
-        pluginDesc = pluginDesc + "\n" + pluginPokemonHomepage;
-        }else{pluginDesc = pluginDesc;};
+        //Pokemon修改author和homepage为图鉴
+        if (iconLibrary2 == "Pokemon" && pluginDesc.search(/^#! *icon *= *.+Pokemon-[0-9]+\.png$/mi) != -1){
+        pluginDesc = pluginDesc.replace(/^#!(?:author|homepage) *= *.*/gmi,'') + "\n" + pluginPokemonAuthor + "\n" + pluginPokemonHomepage;
+        };
 		
     }else{
         if (iconLibrary2 == "Pokemon"){
@@ -871,9 +865,6 @@ if (isLooniOS){
                     pluginDesc = npluginDesc + "\n" + pluginIcon;
         };
     };
-    
-    if (iconReplace == "启用" && pluginDesc.search(/#!icon=/) == -1 ){
-        pluginDesc = pluginDesc + "\n" + pluginIcon};
     
 	script = (script[0] || '') && `[Script]\n\n${script.join("\n\n")}`;
 	
