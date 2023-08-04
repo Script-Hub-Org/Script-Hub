12bf9dfc53086f1917b6f2c1ff424beccd8352b0
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-08-04 10:43:02 +0800

    更新 Loon-Rewrite-Parser.beta.js

--- Loon-Rewrite-Parser.beta.js
+++ Loon-Rewrite-Parser.beta.js
@@ -35,10 +35,8 @@ var hnDel = queryObject.hndel != undefined ? queryObject.hndel.split(/ *, */) : 
 var delNoteSc = istrue(queryObject.del);
 var nCron = queryObject.cron != undefined ? queryObject.cron.split("+") : null;
 var nCronExp = queryObject.cronexp != undefined ? queryObject.cronexp.replace(/\./g," ").split("+") : null;
-var noCache = istrue(queryObject.nocache);
 var nArgTarget = queryObject.arg != undefined ? queryObject.arg.split("+") : null;
 var nArg = queryObject.argv != undefined ? queryObject.argv.split("+") : null;
-var cachExp = queryObject.cachexp != undefined ? queryObject.cachexp : null;
 var jsConverter = queryObject.jsc != undefined ? queryObject.jsc.split("+") : null;
 var jsConverter2 = queryObject.jsc2 != undefined ? queryObject.jsc2.split("+") : null;
 var compatibilityOnly = istrue(queryObject.compatibilityOnly);
@@ -286,25 +284,6 @@ var name = "";
 var desc = "";
 var icon = "";
 
-//缓存有效期相关
-var currentTime = new Date();
-var seconds = Math.floor(currentTime.getTime() / 1000); // 将毫秒转换为秒
-var boxjsSetExp = $.getval("Parser_cache_exp") ?? "1";
-//设置有效期时间
-var expirationTime
-if (cachExp != null){
-  expirationTime = cachExp * 1 * 60 * 60;
-}else{
-  expirationTime = boxjsSetExp * 1 * 60 * 60;
-};
-//$.log(expirationTime);
-var nCache = [{"url":"","body":"","time":""}];
-var oCache = $.getval("parser_cache");
-//检查是否有缓存
-if (oCache != "" && oCache != null){
-  oCache = $.toObj(oCache);
-}else{oCache = null;};
-
 //修改名字和简介
 if (nName === null){
 	name = rewriteName;
@@ -335,64 +314,13 @@ let randomStickerNum = parseInt(stickerStartNum + Math.random() * stickerSum).to
 const pluginIcon = icon;
 //$.log("插件图标：" + pluginIcon);
 
-  let body
-
-  if (noCache == true){
-	body = (await $.http.get(req)).body;
-}else if (oCache == null){
-    //$.log("一个缓存也没有")
-  body = (await $.http.get(req)).body;
-$.log('body:' + body.length + '个字符');
-  nCache[0].url = req;
-  nCache[0].body = body;
-  nCache[0].time = seconds;
-  $.setjson(nCache, 'parser_cache');
-  }else{
-    //删除大于一天的缓存防止缓存越来越大
-    oCache = oCache.filter(obj => {
-  return seconds - obj.time < 86400 ;
-});
-$.setjson(oCache, 'parser_cache');
-
- if (!oCache.some(obj => obj.url === req)){
-     //$.log("有缓存但是没有这个URL的")
-  body = (await $.http.get(req)).body;
-$.log('body:' + body.length + '个字符');
-  nCache[0].url = req;
-  nCache[0].body = body;
-  nCache[0].time = seconds;
-  var mergedCache = oCache.concat(nCache);
-$.setjson(mergedCache, 'parser_cache');
-  }else if (oCache.some(obj => obj.url === req)){
-    const objIndex = oCache.findIndex(obj => obj.url === req);
-    if (seconds - oCache[objIndex].time > expirationTime){
-      //$.log("有缓存且有url,但是过期了")
-  body = (await $.http.get(req)).body;
-$.log('body:' + body.length + '个字符');
-  oCache[objIndex].body = body;
-  oCache[objIndex].time = seconds;
-$.setjson(oCache, 'parser_cache');
-    }else{
-      //$.log("有缓存且有url且没过期")
-    if (oCache[objIndex].body == null || oCache[objIndex].body == ""){
-        //$.log("但是body为null")
-        body = (await $.http.get(req)).body;
-$.log('body:' + body.length + '个字符');
-        oCache[objIndex].body = body;
-        oCache[objIndex].time = seconds;        $.setjson(oCache, "parser_cache");
-    }else{
-        //$.log("获取到缓存body")
-        body = oCache[objIndex].body;
-    }
-      };
-  };
-};
+let body = (await $.http.get(req)).body;
 
 eval(evJsori);
 eval(evUrlori);
 
 if (body.match(/^(?:\s)*\/\*[\s\S]*?(?:\r|\n)\s*\*+\//)){
-	$.log("test")
+	
 body = body.match(/(^(?:\n|\r)*\/\*[\s\S]*?(?:\r|\n)\s*\*+\/)/)[1].match(/[^\r\n]+/g);
 }else{
     body = body.match(/[^\r\n]+/g);};
@@ -503,11 +431,6 @@ jsSuf = `/_end_/_yuliu_.js?type=${oriType}-script&target=${jsTarget}-script&wrap
 
 if (compatibilityOnly == true && (jscStatus == true || jsc2Status == true)){
 jsSuf = jsSuf + "&compatibilityOnly=true"
-};
-
-if (jscStatus == true || jsc2Status == true){
-	noCache == true ? jsSuf = jsSuf + '&nocache=true' : jsSuf = jsSuf;
-	cachExp != null ? jsSuf = jsSuf + `&cachexp=${cachExp}` : jsSuf = jsSuf;
 };
 
 function isJsCon (arr) {
