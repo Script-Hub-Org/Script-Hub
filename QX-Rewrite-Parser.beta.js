05ec9f30e07d5766e22a5276c5047a0ec98e36fe
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-08-04 19:44:58 +0800

    更新 QX-Rewrite-Parser.beta.js

--- QX-Rewrite-Parser.beta.js
+++ QX-Rewrite-Parser.beta.js
@@ -40,14 +40,13 @@ var delNoteSc = istrue(queryObject.del);
 var nCron = queryObject.cron != undefined ? queryObject.cron.split("+") : null;
 var nCronExp = queryObject.cronexp != undefined ? queryObject.cronexp.replace(/\./g," ").split("+") : null;
 var cachExp = queryObject.cachexp != undefined ? queryObject.cachexp : null;
-var noCache = istrue(queryObject.nocache) ?? true;
+var noCache = istrue(queryObject.nocache);
 var jsConverter = queryObject.jsc != undefined ? queryObject.jsc.split("+") : null;
 var jsConverter2 = queryObject.jsc2 != undefined ? queryObject.jsc2.split("+") : null;
 var compatibilityOnly = istrue(queryObject.compatibilityOnly);
 var keepHeader = istrue(queryObject.keepHeader);
 
-var sufnoCache = noCache == true ? `&nocache=true` : "";
-var sufcachExp = cachExp != null ? `&cachexp=${cachExp}` : "";
+var sufkeepHeader = keepHeader == true ? '&keepHeader=true' : '';
 
 const iconStatus = $.getval("启用插件随机图标") ?? "启用";
 const iconReplace = $.getval("替换原始插件图标");
@@ -368,7 +367,6 @@ let reHdType = "";     //request|response-header
 let reHdPtn = "";      //re-header 正则
 let reHdArg1 = "";     //用以匹配的headers
 let reHdArg2 = "";     //替换
-let arg = "";          //echo-response 返回内容
 let echotype = "";     //echo-response 返回类型
 let reBdType = "";     //request|response-body
 let reBdPtn = "";      //re-header 正则
@@ -637,42 +635,40 @@ if (isLooniOS || isSurgeiOS || isShadowrocket){
 				
 			case " echo-response ":
 			
-				arg = x.split(" echo-response ")[2];
-				echotype = x.replace(/\x20{2,}/g," ").split(" echo-response ")[1];
+				js = x.split(" echo-response ")[2];
+				echotype = '&contentType=' + encodeURIComponent(x.replace(/\x20{2,}/g," ").split(" echo-response ")[1]);
 				
-				keepHeader == true ? echotype = `type=${echotype}&` : echotype = "";
-				
-			if (/^(https?|ftp|file).*/.test(arg)){
+			if (/^(https?|ftp|file).*/.test(js)){
 				
 				urlInNum = x.replace(/\x20{2,}/g," ").split(" ").indexOf("url");
 				
 				ptn = x.replace(/\x20{2,}/g," ").split(" ")[urlInNum - 1].replace(/^#/,"");
                 
-				scname = arg.substring(arg.lastIndexOf('/') + 1, arg.lastIndexOf('.') );
+				scname = js.substring(js.lastIndexOf('/') + 1);
 				
-				arg = isSurgeiOS ? arg : encodeURIComponent(arg) + sufcachExp + sufnoCache;
+				js = isSurgeiOS ? js : `http://script.hub/convert/_start_/${js}/_end_/${scname}?type=mock${echotype}${sufkeepHeader}`;
 				if (isLooniOS){
 				body[y - 1]?.match(/^#/) && script.push(body[y - 1]);
 				
 				script.push(
-					`${noteK}http-request ${ptn} script-path=https://raw.githubusercontent.com/Script-Hub-Org/Script-Hub/main/scripts/echo-response.js, timeout=60, tag=${scname}_${y}, argument=${echotype}url=${arg}`);
+					`${noteK}http-request ${ptn} script-path=${js}, timeout=60, tag=${scname}_${y}`);
 				}else if (isSurgeiOS){
 				body[y - 1]?.match(/^#/) && MapLocal.push(body[y - 1]);
 				
-				MapLocal.push(`${noteK}${ptn} data="${arg}"`);
+				MapLocal.push(`${noteK}${ptn} data="${js}"`);
 				}else if (isShadowrocket){
 				body[y - 1]?.match(/^#/) && script.push(body[y - 1]);
 				
 				script.push(
-					`${noteK}${scname}_${y} = type=http-request, pattern=${ptn}, script-path=https://raw.githubusercontent.com/Script-Hub-Org/Script-Hub/main/scripts/echo-response.js, timeout=60, argument=${echotype}url=${arg}`)
+					`${noteK}${scname}_${y} = type=http-request, pattern=${ptn}, script-path=${js}, timeout=60`)
 				}else if (isStashiOS){
 				body[y - 1]?.match(/^#/) && script.push("    " + body[y - 1]);
 				
 				script.push(
-					`${noteK4}- match: ${ptn}${noteKn6}name: "echo-response"${noteKn6}type: request${noteKn6}timeout: 30${noteKn6}argument: |-${noteKn8}${echotype}url=${arg}`)
+					`${noteK4}- match: ${ptn}${noteKn6}name: "${scname}_${y}"${noteKn6}type: request${noteKn6}timeout: 60`)
 				
 				providers.push(
-							`${noteK2}"echo-response":${noteKn4}url: https://raw.githubusercontent.com/Script-Hub-Org/Script-Hub/main/scripts/echo-response.js${noteKn4}interval: 86400`);
+							`${noteK2}"${scname}_${y}":${noteKn4}url: ${js}${noteKn4}interval: 86400`);
 				}; 
 
 			}else{others.push(x)};
