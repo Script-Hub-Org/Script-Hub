c84a97685c2888faace714f3c8f3caf43297a558
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-08-04 21:33:53 +0800

    更新 QX-Rewrite-Parser.js

--- QX-Rewrite-Parser.js
+++ QX-Rewrite-Parser.js
@@ -39,11 +39,15 @@ var hnDel = queryObject.hndel != undefined ? queryObject.hndel.split(/ *, */) : 
 var delNoteSc = istrue(queryObject.del);
 var nCron = queryObject.cron != undefined ? queryObject.cron.split("+") : null;
 var nCronExp = queryObject.cronexp != undefined ? queryObject.cronexp.replace(/\./g," ").split("+") : null;
-var cachExp = queryObject.cachexp != undefined ? queryObject.cachexp : null;
-var noCache = istrue(queryObject.nocache);
 var jsConverter = queryObject.jsc != undefined ? queryObject.jsc.split("+") : null;
 var jsConverter2 = queryObject.jsc2 != undefined ? queryObject.jsc2.split("+") : null;
 var compatibilityOnly = istrue(queryObject.compatibilityOnly);
+var keepHeader = istrue(queryObject.keepHeader);
+var jsDelivr = istrue(queryObject.jsDelivr);
+
+var sufkeepHeader = keepHeader == true ? '&keepHeader=true' : '';
+var sufjsDelivr = jsDelivr == true ? '&jsDelivr=true' : '';
+
 const iconStatus = $.getval("启用插件随机图标") ?? "启用";
 const iconReplace = $.getval("替换原始插件图标");
 const iconLibrary1 = $.getval("插件随机图标合集") ?? "Doraemon(100P)";
@@ -295,25 +299,6 @@ var name = "";
 var desc = "";
 var icon = "";
 
-//缓存有效期相关
-var currentTime = new Date();
-var seconds = Math.floor(currentTime.getTime() / 1000); // 将毫秒转换为秒
-var boxjsSetExp = $.getval("Parser_cache_exp") ?? "1";
-//设置有效期时间
-var expirationTime
-if (cachExp != null){
-  expirationTime = cachExp * 1 * 60 * 60;
-}else{
-  expirationTime = boxjsSetExp * 1 * 60 * 60;
-};
-//$.log(expirationTime);
-var nCache = [{"url":"","body":"","time":""}];
-var oCache = $.getval("parser_cache");
-//检查是否有缓存
-if (oCache != "" && oCache != null){
-  oCache = $.toObj(oCache);
-}else{oCache = null;};
-
 //修改名字和简介
 if (nName === null){
 	name = rewriteName;
@@ -343,63 +328,13 @@ let randomStickerNum = parseInt(stickerStartNum + Math.random() * stickerSum).to
 const pluginIcon = icon;
 //$.log("插件图标：" + pluginIcon);
 
-  let body
+let body = (await $.http.get(req)).body;
 
-  if (noCache == true){
-	body = (await $.http.get(req)).body;
-}else if (oCache == null){
-    //$.log("一个缓存也没有")
-  body = (await $.http.get(req)).body;
-$.log('body字节数:' + body.length + '字');
-  nCache[0].url = req;
-  nCache[0].body = body;
-  nCache[0].time = seconds;
-  $.setjson(nCache, 'parser_cache');
-  }else{
-    //删除大于一天的缓存防止缓存越来越大
-    oCache = oCache.filter(obj => {
-  return seconds - obj.time < 86400 ;
-});
-$.setjson(oCache, 'parser_cache');
-
- if (!oCache.some(obj => obj.url === req)){
-     //$.log("有缓存但是没有这个URL的")
-  body = (await $.http.get(req)).body;
-$.log('body字节数:' + body.length + '字');
-  nCache[0].url = req;
-  nCache[0].body = body;
-  nCache[0].time = seconds;
-  var mergedCache = oCache.concat(nCache);
-$.setjson(mergedCache, 'parser_cache');
-  }else if (oCache.some(obj => obj.url === req)){
-    const objIndex = oCache.findIndex(obj => obj.url === req);
-    if (seconds - oCache[objIndex].time > expirationTime){
-      //$.log("有缓存且有url,但是过期了")
-  body = (await $.http.get(req)).body;
-$.log('body字节数:' + body.length + '字');
-  oCache[objIndex].body = body;
-  oCache[objIndex].time = seconds;
-$.setjson(oCache, 'parser_cache');
-    }else{
-      //$.log("有缓存且有url且没过期")
-    if (oCache[objIndex].body == null || oCache[objIndex].body == ""){
-        //$.log("但是body为null")
-        body = (await $.http.get(req)).body;
-$.log('body字节数:' + body.length + '字');
-        oCache[objIndex].body = body;
-        oCache[objIndex].time = seconds;        $.setjson(oCache, "parser_cache");
-    }else{
-        //$.log("获取到缓存body")
-        body = oCache[objIndex].body;
-    }
-      };
-  };
-};
 eval(evJsori);
 eval(evUrlori);
 
 if (body.match(/^(?:\s)*\/\*[\s\S]*?(?:\r|\n)\s*\*+\//)){
-	$.log("test")
+
 body = body.match(/(^(?:\n|\r)*\/\*[\s\S]*?(?:\r|\n)\s*\*+\/)/)[1].match(/[^\r\n]+/g);
 }else{
     body = body.match(/[^\r\n]+/g);};
@@ -432,9 +367,7 @@ let reHdType = "";     //request|response-header
 let reHdPtn = "";      //re-header 正则
 let reHdArg1 = "";     //用以匹配的headers
 let reHdArg2 = "";     //替换
-let arg = "";          //echo-response 返回内容
-let mockPtn = "";      //echo-res转mock 正则
-let dataCon = "";      //echo-res转mock 返回内容
+let echotype = "";     //echo-response 返回类型
 let reBdType = "";     //request|response-body
 let reBdPtn = "";      //re-header 正则
 let reBdArg1 = "";     //用以匹配的headers
@@ -499,6 +432,7 @@ let jsPre = "";
 let jsSuf = "";
 let oriType = queryObject.type.split("-")[0];
 let jsTarget = queryObject.target.split("-")[0];
+
 if (jscStatus == true || jsc2Status == true){
 jsPre = "http://script.hub/convert/_start_/";
 };
@@ -507,9 +441,11 @@ jsSuf = `/_end_/_yuliu_.js?type=${oriType}-script&target=${jsTarget}-script`;
 }else if (jsc2Status == true){
 jsSuf = `/_end_/_yuliu_.js?type=${oriType}-script&target=${jsTarget}-script&wrap_response=true`;
 };
+
 if (compatibilityOnly == true && (jscStatus == true || jsc2Status == true)){
 jsSuf = jsSuf + "&compatibilityOnly=true"
 };
+
 function isJsCon (arr) {
 	if (arr != null){
 		for (let i=0; i < arr.length; i++) {
@@ -590,8 +526,6 @@ if (isLooniOS || isSurgeiOS || isShadowrocket){
 				js = x.replace(/\x20{2,}/g," ").split(" ")[urlInNum + 2];
 				
 				scname = js.substring(js.lastIndexOf('/') + 1, js.lastIndexOf('.') );
-				
-				jsname = scname;
 
 				if (isSurgeiOS){
 					ptn = ptn.replace(/(.+,.+)/,'"$1"');};
@@ -612,8 +546,8 @@ if (isLooniOS || isSurgeiOS || isShadowrocket){
 				
 				size = x.match(/\x20script[^\s]*(-body|-analyze)/) ? '3145728' : '0';
 				};
-//开启脚本转换				
-				js = jsPre + js + jsSuf.replace(/_yuliu_/,jsname);
+
+				js = toJsc(js);
 				
 				if (isLooniOS){			
 				body[y - 1]?.match(/^#/) && script.push(body[y - 1]);
@@ -701,41 +635,40 @@ if (isLooniOS || isSurgeiOS || isShadowrocket){
 				
 			case " echo-response ":
 			
-				arg = x.split(" echo-response ")[2];
-			
-			if(/^(https?|ftp|file):\/\/.*/.test(arg)){
+				js = x.split(" echo-response ")[2];
+				echotype = '&contentType=' + encodeURIComponent(x.replace(/\x20{2,}/g," ").split(" echo-response ")[1]);
+				
+			if (/^(https?|ftp|file).*/.test(js)){
 				
 				urlInNum = x.replace(/\x20{2,}/g," ").split(" ").indexOf("url");
 				
 				ptn = x.replace(/\x20{2,}/g," ").split(" ")[urlInNum - 1].replace(/^#/,"");
                 
-				scname = arg.substring(arg.lastIndexOf('/') + 1, arg.lastIndexOf('.') );
+				scname = js.substring(js.lastIndexOf('/') + 1);
+				
+				js = isSurgeiOS ? js : `http://script.hub/convert/_start_/${js}/_end_/${scname}?type=mock${echotype}${sufkeepHeader}${sufjsDelivr}`;
 				if (isLooniOS){
 				body[y - 1]?.match(/^#/) && script.push(body[y - 1]);
 				
 				script.push(
-					`${noteK}http-request ${ptn} script-path=https://raw.githubusercontent.com/Script-Hub-Org/Script-Hub/main/scripts/echo-response.js, timeout=60, tag=${scname}_${y}, argument=type=text/json&url=${arg}`);
+					`${noteK}http-request ${ptn} script-path=${js}, timeout=60, tag=${scname}_${y}`);
 				}else if (isSurgeiOS){
 				body[y - 1]?.match(/^#/) && MapLocal.push(body[y - 1]);
-
-				mockPtn = x.replace(/\x20{2,}/g," ").split(" url echo-response")[0].replace(/^#/,"");
 				
-				dataCon = x.replace(/\x20{2,}/g," ").split(" echo-response ")[2];
-				
-				MapLocal.push(`${noteK}${mockPtn} data="${dataCon}"`);
+				MapLocal.push(`${noteK}${ptn} data="${js}"`);
 				}else if (isShadowrocket){
 				body[y - 1]?.match(/^#/) && script.push(body[y - 1]);
 				
 				script.push(
-					`${noteK}${scname}_${y} = type=http-request, pattern=${ptn}, script-path=https://raw.githubusercontent.com/Script-Hub-Org/Script-Hub/main/scripts/echo-response.js, timeout=60, argument=type=text/json&url=${arg}`)
+					`${noteK}${scname}_${y} = type=http-request, pattern=${ptn}, script-path=${js}, timeout=60`)
 				}else if (isStashiOS){
 				body[y - 1]?.match(/^#/) && script.push("    " + body[y - 1]);
 				
 				script.push(
-					`${noteK4}- match: ${ptn}${noteKn6}name: "echo-response"${noteKn6}type: request${noteKn6}timeout: 30${noteKn6}argument: |-${noteKn8}type=text/json&url=${arg}`)
+					`${noteK4}- match: ${ptn}${noteKn6}name: "${scname}_${y}"${noteKn6}type: request${noteKn6}timeout: 60`)
 				
 				providers.push(
-							`${noteK2}"echo-response":${noteKn4}url: https://raw.githubusercontent.com/Script-Hub-Org/Script-Hub/main/scripts/echo-response.js${noteKn4}interval: 86400`);
+							`${noteK2}"${scname}_${y}":${noteKn4}url: ${js}${noteKn4}interval: 86400`);
 				}; 
 
 			}else{others.push(x)};
@@ -821,9 +754,7 @@ if (isLooniOS || isSurgeiOS || isShadowrocket){
 				
 				croName = cronJs.substring(cronJs.lastIndexOf('/') + 1, cronJs.lastIndexOf('.') );
 				
-				jsname = croName;
-				
-				cronJs = jsPre + cronJs + jsSuf.replace(/_yuliu_/,jsname);
+				cronJs = toJsc(cronJs);
 				
 				if (isSurgeiOS || isShadowrocket){
 				body[y - 1]?.match(/^#/) && script.push(body[y - 1]);
@@ -838,7 +769,16 @@ if (isLooniOS || isSurgeiOS || isShadowrocket){
 					
 scriptBox.push({"noteK":noteKstatus,"jsurl":cronJs,"name":croName + "_" + y,"cron":cronExp});	};
                     };//定时任务转换结束
-				}
+				}			
+//开启脚本转换
+function toJsc (js) {
+	if (jscStatus == true || jsc2Status == true){
+				jsname = js.substring(js.lastIndexOf('/') + 1, js.lastIndexOf('.') );
+                		
+				return js = jsPre + js + jsSuf.replace(/_yuliu_/,jsname);
+		
+	}else{return js}
+};	
 		} //switch结束
 	
 }; //循环结束
