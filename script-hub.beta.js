987ebc4f5c70cb762455d6e26a03dee30c9e742c
xream <xream@icloud.com>
2024-01-07 20:18:21 +0800

    [v] v1.14.8

--- script-hub.beta.js
+++ script-hub.beta.js
@@ -1161,7 +1161,7 @@ const htmls = `
 
     </div>
     <footer>
-      <p>Made With &hearts; By <a href="https://github.com/Script-Hub-Org/Script-Hub">Script Hub v1.14.7</a></p>
+      <p>Made With &hearts; By <a href="https://github.com/Script-Hub-Org/Script-Hub">Script Hub v1.14.8</a></p>
     </footer>
     <script>
       const openAllDetails = () => document.querySelectorAll('details').forEach(i => i.setAttribute('open', ""))
