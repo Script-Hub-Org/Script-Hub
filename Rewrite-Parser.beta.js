ba20334b4dc89e277e5c25aef3b903cb94986d61
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2024-01-30 22:34:45 +0800

    修改Surge的Panel解析输出逻辑

--- Rewrite-Parser.beta.js
+++ Rewrite-Parser.beta.js
@@ -104,6 +104,11 @@ let name,
   modistatus,
   hostdomain,
   hostvalue,
+  panelname,
+  title,
+  content,
+  style,
+  scriptname,
   jsurl,
   jsname,
   jsfrom,
@@ -184,6 +189,7 @@ let hostBox = [] //host
 let ruleBox = [] //规则
 let rwBox = [] //重写
 let rwhdBox = [] //HeaderRewrite
+let panelBox = [] //Panel信息
 let jsBox = [] //脚本
 let mockBox = [] //MapLocal或echo-response
 let hnBox = [] //MITM主机名
@@ -215,8 +221,11 @@ let providers = []
 
 hnBox = hnAdd != null ? hnAdd : []
 
-const jsRegx =
+const jsRegex =
   /\s*[=,]\s*(?:script-path|pattern|timeout|argument|script-update-interval|requires-body|max-size|ability|binary-body-mode|cronexpr?|wake-system|enabled?|tag|type|img-url|debug|event-name|desc)\s*=\s*/
+
+const panelRegex =
+  /\s*[=,]\s*(?:title|content|style|script-name|update-interval)\s*=\s*/
 
 //查询js binarymode相关
 let binaryInfo = $.getval('Parser_binary_info')
@@ -433,6 +442,30 @@ if (binaryInfo != null && binaryInfo.length > 0) {
       hostvalue = x.split(/\s*=\s*/)[1]
       hostBox.push({ mark, noteK, hostdomain, hostvalue, ori: x })
     }
+    
+    //Panel信息
+    if (/[=,]\s*script-name\s*=.+/.test(x)) {
+      mark = getMark(y, body)
+      noteK = isNoteK(x)
+      panelname = x.split(/\s*=/)[0].replace(/^#/, '')
+      title = getJsInfo(x, /[=,\s]\s*title\s*=\s*/)
+      content = getJsInfo(x, /[=,\s]\s*content\s*=\s*/)
+      style = getJsInfo(x, /[=,\s]\s*style\s*=\s*/)
+      scriptname = getJsInfo(x, /[=,\s]\s*script-name\s*=\s*/)
+      updatetime = getJsInfo(x, /[=,\s]\s*update-interval\s*=\s*/)
+      panelBox.push({
+        mark,
+        noteK,
+        panelname,
+        title,
+        content,
+        style,
+        scriptname,
+        updatetime,
+        ori: x,
+        num: y,
+      })  
+    }//Panel信息解析结束
 
     //脚本解析
     if (/script-path\s*=.+/.test(x)) {
@@ -906,6 +939,34 @@ if (binaryInfo != null && binaryInfo.length > 0) {
     } //switch
   } //Mock输出for
 
+  //Panel输出
+  if (isSurgeiOS && panelBox.length > 0) {
+    for (let i = 0; i < panelBox.length; i++) {
+      noteK = panelBox[i].noteK ? '#' : ''
+      mark = panelBox[i].mark ? panelBox[i].mark : ''
+      panelname = panelBox[i].panelname
+      title = panelBox[i].title ? ', title=' + panelBox[i].title : ''
+      content = panelBox[i].content ? ', content=' + panelBox[i].content : ''
+      style = panelBox[i].style ? ',style=' + panelBox[i].style : ''
+      scriptname = panelBox[i].scriptname
+      updatetime = panelBox[i].updatetime ? ', update-interval=' + panelBox[i].updatetime : ''
+      ori = panelBox[i].ori
+      scriptname = reJsValue(njsnametarget || 'null', njsname, scriptname, ori, scriptname)
+      Panel.push(
+              mark +
+                noteK +
+                panelname +
+                ' = ' +
+                'script-name=' +
+                scriptname +
+                title +
+                content +
+                style +
+                updatetime
+            )
+    }//for
+  }//panel输出结束
+
   //脚本输出
   if (!isStashiOS && jsBox.length > 0) {
     for (let i = 0; i < jsBox.length; i++) {
@@ -1043,10 +1104,6 @@ if (binaryInfo != null && binaryInfo.length > 0) {
             )
           } else {
             otherRule.push(jsBox[i].ori)
-          }
-
-          if (isSurgeiOS && jstype == 'generic') {
-            Panel.push(noteK + jsname + ' = script-name=' + jsname + ', update-interval=3600')
           }
           break
       } //switch
@@ -1392,11 +1449,12 @@ function rw_redirect(x, mark) {
 }
 
 //script
-function getJsInfo(x, regx) {
-  if (regx.test(x)) {
+function getJsInfo(x, regex) {
+  let parserRegex = /script-name\s*=/.test(x) ? panelRegex : jsRegex
+  if (regex.test(x)) {
     return x
-      .split(regx)[1]
-      .split(jsRegx)[0]
+      .split(regex)[1]
+      .split(parserRegex)[0]
   } else {
     return ''
   }
