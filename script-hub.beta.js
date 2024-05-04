ac586bc6514c750c778a48987768dda397182ff0
xream <xream@icloud.com>
2024-05-04 17:04:32 +0800

    release: version 1.14.12

--- script-hub.beta.js
+++ script-hub.beta.js
@@ -1215,7 +1215,7 @@ const htmls = `
 
     </div>
     <footer>
-      <p>Made With &hearts; By <a href="https://github.com/Script-Hub-Org/Script-Hub">Script Hub v1.14.11</a></p>
+      <p>Made With &hearts; By <a href="https://github.com/Script-Hub-Org/Script-Hub">Script Hub v1.14.12</a></p>
     </footer>
     <script>
       const openAllDetails = () => document.querySelectorAll('details').forEach(i => i.setAttribute('open', ""))
